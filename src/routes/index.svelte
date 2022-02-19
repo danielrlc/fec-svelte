923aff134595300868fe5a12d1eab36effe6bdbe
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-02-20 01:50:55 +0200

    Toggle words in example sentence

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,23 @@
+<script>
+  let sentence = "Mikä sinun nimesi on?";
+  let wordStatusRegister = sentence.split(" ").map((word) => true);
+  function toggleWord(i) {
+    wordStatusRegister[i] = !wordStatusRegister[i];
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+  {#each sentence.split(" ") as word, i}
+    <span on:click={e => toggleWord(i)}
+      >{wordStatusRegister[i] ? word : "____ "}
+    </span>
+  {/each}
+</p>
+
+<style>
+  p {
+    font-size: 1.5rem;
+    font-family: monospace;
+  }
+</style>
