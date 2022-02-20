e7754057721cd7f6621cbadd8237a2734f5daa75
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-02-20 21:55:04 +0200

    Tailwind with purging // Toggle words in any line

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,23 +1,36 @@
 <script>
-  let sentence = "Mikä sinun nimesi on?";
-  let wordStatusRegister = sentence.split(" ").map((word) => true);
-  function toggleWord(i) {
-    wordStatusRegister[i] = !wordStatusRegister[i];
+  let dialogue = [
+    [`John`, `Hello? Can I help you?`],
+    [`Ludwig`, `Are you Bertrand Russell, the greatest thinker of the age?`],
+  ];
+  let speaker = 0
+  let speech = 1
+  let wordStatusRegister = dialogue.map((speech) =>
+    speech[1].split(" ").map((word) => true)
+  );
+  let hintsAreOn = false;
+  function toggleWord(i, j) {
+    wordStatusRegister[i][j] = !wordStatusRegister[i][j];
   }
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  {#each sentence.split(" ") as word, i}
-    <span on:click={e => toggleWord(i)}
-      >{wordStatusRegister[i] ? word : "____ "}
-    </span>
-  {/each}
-</p>
-
-<style>
-  p {
-    font-size: 1.5rem;
-    font-family: monospace;
-  }
-</style>
+<div class="text-center text-2xl py-4">
+  <h1 class="font-bold mb-8">Flippin' English Club</h1>
+  <p class="font-mono">
+    {#each dialogue as line, i}
+      <span class="font-bold">{line[speaker]}: </span>
+      {#each line[speech].split(" ") as word, j}
+        <span on:click={(e) => toggleWord(i, j)}
+          >{wordStatusRegister[i][j]
+            ? word
+            : hintsAreOn
+            ? word
+                .split("")
+                .map((letter, k) => (k === 0 ? letter : "_"))
+                .join("")
+            : "____ "}
+        </span>
+      {/each}
+    {/each}
+  </p>
+</div>
