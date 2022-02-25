7895a11967bc8f967b6616096c726363af17e9d0
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-02-25 16:56:09 +0200

    Loop through TOC data on home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,20 @@
 <script>
   import Header from "../components/Header.svelte";
+
+  let pages = [
+    {
+      title: "#1 - Ludwig turns up at the wrong door",
+      slug: "1-ludwig-turns-up-at-the-wrong-door",
+    },
+    {
+      title: "#2 - Spoken phrases (placeholder content)",
+      slug: "2-spoken-phrases",
+    },
+  ];
 </script>
 
-<Header/>
-<a class="block underline px-3 mb-4" href="/ex/001-ludwig-turns-up-at-the-wrong-door"
-  >#1 - Ludwig turns up at the wrong door</a
->
-<a class="block underline px-3 mb-4" href="/ex/002-spoken-phrases"
-  >#2 - Spoken phrases (coming soon - placeholder phrases for now)</a
->
+<Header />
+
+{#each pages as { title, slug }}
+  <a class="block underline px-3 mb-4" href="/ex/{slug}">{title}</a>
+{/each}
