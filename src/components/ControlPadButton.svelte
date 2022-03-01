3cad8787783981d9326a9fd494b1ca93fb7b1e0f
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-03-01 22:46:55 +0200

    Progress map // First Tailwind components

--- src/components/ControlPadButton.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  export let action
-  export let label
-</script>
-
-<button class="w-11 mx-1 py-2 bg-green-300 rounded-md" on:click={action}
-  >{label}</button
->
--- src/components/Flashcards.svelte
+++ src/components/Flashcards.svelte
@@ -1,7 +1,6 @@
 <script>
   import Header from "./Header.svelte";
   import Title from "./Title.svelte";
-  import ControlPadButton from "./ControlPadButton.svelte";
 
   export let title;
   export let sourcePhrases;
@@ -11,6 +10,9 @@
   let wordI = 0;
   let hintsAreOn = false;
   let phraseCount = sourcePhrases.length;
+  let introIsShown = false;
+  let flashcardsAreShown = true;
+  let progressMapIsShown = false;
 
   let wordStatusStore = targetPhrases.map((phrase) =>
     phrase.split(" ").map((_) => false)
@@ -24,28 +26,29 @@
       : "learning"
   );
 
+  $: wholePhraseIsHidden = phraseStatusStore[phraseI] === "tolearn";
+  $: wholePhraseIsShown = phraseStatusStore[phraseI] === "learnt";
+
+  $: progress = Math.round(
+    (phraseStatusStore.filter((phrase) => phrase === "learnt").length /
+      phraseCount) *
+      100
+  );
+
   function toggleWord(i) {
     wordI = i;
     wordStatusStore[phraseI][wordI] = !wordStatusStore[phraseI][wordI];
   }
 
-  function showWord() {
+  function showWordAndGoToNext() {
     wordStatusStore[phraseI][wordI] = true;
-  }
-
-  function goToNextWord() {
-    if (wordI < wordStatusStore[phraseI].length - 1) {
-      wordI++;
-    }
+    wordI < wordStatusStore[phraseI].length - 1 && wordI++;
   }
 
   function showThreeWords() {
-    showWord();
-    goToNextWord();
-    showWord();
-    goToNextWord();
-    showWord();
-    goToNextWord();
+    showWordAndGoToNext();
+    showWordAndGoToNext();
+    showWordAndGoToNext();
   }
 
   function showPhrase() {
@@ -55,10 +58,6 @@
   function hidePhrase() {
     wordStatusStore[phraseI] = wordStatusStore[phraseI].map((_) => false);
   }
-
-  $: wholePhraseIsHidden = wordStatusStore[phraseI].every(
-    (status) => status === false
-  );
 
   function togglePhrase() {
     wholePhraseIsHidden ? showPhrase() : hidePhrase();
@@ -74,18 +73,26 @@
     wordI = 0;
   }
 
-  $: progress = Math.round(
-    (phraseStatusStore.filter((phrase) => phrase === "learnt").length /
-      phraseCount) *
-      100
-  );
-
-  let introIsShown = true;
-  let flashcardsAreShown = false;
-
   function endIntro() {
     introIsShown = false;
     flashcardsAreShown = true;
+  }
+
+  function showFlashcards() {
+    introIsShown = false;
+    flashcardsAreShown = true;
+    progressMapIsShown = false;
+  }
+
+  function showProgressMap() {
+    introIsShown = false;
+    flashcardsAreShown = false;
+    progressMapIsShown = true;
+  }
+
+  function selectPhraseFromProgressMap(i) {
+    phraseI = i;
+    showFlashcards();
   }
 </script>
 
@@ -98,9 +105,39 @@
       <button on:click={endIntro} class="mx-3 rounded-md bg-green-300 px-8 py-2"
         >Start</button
       >
-    {:else}
-      <div>
-        <p class="px-3 my-5 leading-5">{sourcePhrases[phraseI]}</p>
+    {/if}
+
+    {#if progressMapIsShown}
+      <div class="grid content-center py-4 h-full text-center">
+        <h2 class="text-xl font-bold mb-2">Progress map</h2>
+        <p class="mb-6 text-sm">Click on a phrase to jump to it.</p>
+
+        <div
+          class="grid grid-cols-10 gap-x-1 gap-y-4 p-3 content-center h-full"
+        >
+          {#each phraseStatusStore as phraseStatus, i}
+            <button
+              class="c-flex-center w-7 h-10 rounded-md {phraseStatus ===
+              'tolearn'
+                ? 'bg-red-300'
+                : phraseStatus === 'learning'
+                ? 'bg-yellow-300'
+                : 'bg-green-300'} {i === phraseI && 'border-4 border-gray-700'}"
+              on:click={(_) => selectPhraseFromProgressMap(i)}
+              >{phraseStatus === "tolearn"
+                ? "O"
+                : phraseStatus === "learning"
+                ? "..."
+                : "X"}</button
+            >
+          {/each}
+        </div>
+      </div>
+    {/if}
+
+    {#if flashcardsAreShown}
+      <div class="text-lg">
+        <p class="px-3 my-5 leading-6">{sourcePhrases[phraseI]}</p>
         <hr class="mb-4" />
         <div class="px-3 leading-7">
           {#each targetPhrases[phraseI].split(" ") as word, i}
@@ -117,19 +154,51 @@
                     .join("")
                 : "_____"}
             </span>
+            <span />
           {/each}
         </div>
       </div>
     {/if}
   </div>
-  <div class="p-2 bg-gray-700 text-center">
-    <div>
-      <span class="text-white mr-1">{phraseI + 1}/{phraseCount}</span>
-      <ControlPadButton action={toggleHints} label="Hints" />
-      <ControlPadButton action={showThreeWords} label="+3" />
-      <ControlPadButton action={togglePhrase} label="All" />
-      <ControlPadButton action={goForward1} label=">" />
-      <span class="text-white ml-1">{progress}%</span>
+
+  {#if flashcardsAreShown}
+    <div class="p-2 bg-gray-700 text-center">
+      <div class="c-flex-center">
+        <span class="text-white mr-2">{phraseI + 1}/{phraseCount}</span>
+
+        <button
+          class="c-flashcards-btn bg-green-300 {wholePhraseIsShown &&
+            'c-disabled'}"
+          on:click={toggleHints}>{hintsAreOn ? "No hints" : "Hints"}</button
+        >
+
+        <button class="c-flashcards-btn bg-green-300" on:click={showThreeWords}
+          >+3</button
+        >
+
+        <button
+          class="c-flashcards-btn {wholePhraseIsHidden
+            ? 'bg-red-300'
+            : wholePhraseIsShown
+            ? 'bg-green-300'
+            : 'bg-yellow-300'}"
+          on:click={togglePhrase}>{wholePhraseIsHidden ? "All" : "None"}</button
+        >
+
+        <button class="c-flashcards-btn bg-green-300" on:click={goForward1}
+          >></button
+        >
+
+        <button
+          class="c-flashcards-btn bg-green-300"
+          on:click={showProgressMap}>Map</button
+        >
+
+        <span class="text-white ml-2">{progress}%</span>
+      </div>
     </div>
-  </div>
+  {/if}
 </div>
+
+<style>
+</style>
