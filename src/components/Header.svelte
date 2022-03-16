db4143d332cdc13d97d9bc09cc3494b4f305f99f
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-03-16 20:31:09 +0200

    Test going full screen

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,12 @@
-<div class="bg-blue-300 py-1 px-3 lg:px-8 mb-6 lg:mb-10">
-  <a href="/" class="font-bold text-center hover:underline"
+<script>
+  function goFullScreen() {
+    document.body.requestFullscreen();
+  }
+</script>
+
+<div class="mb-6 bg-blue-300 py-1 px-3 lg:mb-10 lg:px-8">
+  <a href="/" class="text-center font-bold hover:underline"
     >Flippin' English Club</a
   >
+  <button on:click={goFullScreen}>Go fullscreen</button>
 </div>
