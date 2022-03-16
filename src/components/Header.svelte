745c9afb018ffcb881d0ce13dda550d325a1fe82
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-03-16 20:53:57 +0200

    Remove fullscreen attempt (saved in other branch)

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,5 @@
-<script>
-  function goFullScreen() {
-    document.body.requestFullscreen();
-  }
-</script>
-
 <div class="mb-6 bg-blue-300 py-1 px-3 lg:mb-10 lg:px-8">
   <a href="/" class="text-center font-bold hover:underline"
     >Flippin' English Club</a
   >
-  <button on:click={goFullScreen}>Go fullscreen</button>
 </div>
