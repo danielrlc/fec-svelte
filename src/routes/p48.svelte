6b94044e7ebb20e65cd2ea1bdd8850a630500c30
Daniel Clarke <daniel.clarke@ksfmedia.fi>
2022-03-17 09:11:41 +0200

    Add two fin exs

---
+++ src/routes/p48.svelte
@@ -0,0 +1,53 @@
+<script>
+  import Flashcards from "../components/Flashcards.svelte";
+
+  const content = {
+    title: `#3 - Hei Hanna! Tänään alkaa Helsinki-viikko! (p48)`,
+
+    targetPhrases: [
+      `Hei Hanna!`,
+      `Tänään alkaa Helsinki-viikko!`,
+      `Mennään katsomaan jotain esitystä!`,
+      `Joo, mennään vaan.`,
+      `Mitä ohjelmaa siel on?`,
+      `Vaikka mitä?`,
+      `Keskustassa on ainakin teatteria ja konsertti.`,
+      `Vai lähdetäänks pelaamaan rantalentistä?`,
+      `Mennään vaan!`,
+      `Olis kiva pelata, kun on näin hyvä ilmakin.`,
+      `Joo, tehdään niin.`,
+      `Ja illalla katsotaan sit leffa Senaatintorilla!`,
+      `Ai ulkona, vau!`,
+      `Mikä leffa siel näytetään?`,
+      `Joku Kaurismäen filmi.`,
+      `Mä en ole vielä nähnyt yhtään Kaurismäen leffaa.`,
+      `Siksi mä haluaisin nähdä tän.`,
+      `Okei, sopii mulle.`,
+      `Täytyy varmaan mennä ajoissa, et saadaan hyvät paikat.`,
+      `Hey Hanna!`,
+    ],
+
+    sourcePhrases: [
+      `Helsinki Week starts today!`,
+      `Let's go see a show!`,
+      `Yeah, let's just go.`,
+      `What's on the program?`,
+      `All kinds?`,
+      `There is at least some theatre and a concert in the centre.`,
+      `Or let's start playing on the beach?`,
+      `Let's go!`,
+      `It would be nice to play when the weather is so good.`,
+      `Yeah, let's do that.`,
+      `And in the evening, watch a movie at Senate Square!`,
+      `Oh outside, Wow!`,
+      `Which movie will be shown?`,
+      `Some Kaurismäki film.`,
+      `I haven't seen any Kaurismäki movies yet.`,
+      `That's why I'd like to see this.`,
+      `Okay, that sounds good to me.`,
+      `Let's go in good time, or we won't get good places.`,
+    ],
+  };
+</script>
+
+<Flashcards {...content} />
---
+++ src/routes/toc.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Header from "../components/Header.svelte";
+
+  let toc = [
+    {
+      title: "#1 - Ch1 various",
+      slug: "p1",
+    },
+    {
+      title: "#2 - Ohjelmaa aamusta iltaan (p47)",
+      slug: "p47",
+    },
+    {
+      title: "#3 - Hei Hanna! Tänään alkaa Helsinki-viikko! (p48)",
+      slug: "p48",
+    },
+  ];
+</script>
+
+<Header />
+
+<div class="flex justify-center text-[1.1rem]">
+  <div class="w-[800px]">
+    <h1 class="text-lg font-bold px-3 mb-4">Exercises</h1>
+    {#each toc as { title, slug }}
+      <a class="block underline px-3 mb-1" href={slug}>{title}</a>
+    {/each}
+  </div>
+</div>
